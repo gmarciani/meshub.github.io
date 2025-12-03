4bd82d72437535c05ef0d7e7165cd9bdbd9d9cf5
Giacomo Marciani <mgiacomo@amazon.com>
2025-12-02 23:30:12 -0500

    Changed logo and added GA tracking

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -15,10 +15,12 @@ export default function GoogleAnalytics() {
         src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
       />
       <Script id="google-analytics">
-        {`window.dataLayer = window.dataLayer || [];
-function gtag() {dataLayer.push(arguments);}
-gtag("js", new Date());
-gtag("config", "${measurementId}");`}
+        {
+        `window.dataLayer = window.dataLayer || [];
+        function gtag() {dataLayer.push(arguments);}
+        gtag("js", new Date());
+        gtag("config", "${measurementId}");`
+      }
       </Script>
     </>
   );
